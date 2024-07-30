5a9f21763eee2631a48bf03dc0d30f5516e07def
sadsax7 <aarangom1@eafit.edu.co>
2024-07-30 13:17:27 -0500

    cambios

--- movie/views.py
+++ movie/views.py
@@ -10,6 +10,6 @@ def home(request):
 
 
 def about(request):
-    return HttpResponse('<h1> Welcome to About Page </h1>')
-    #return render(request, 'about.html')
+    #return HttpResponse('<h1> Welcome to About Page </h1>')
+    return render(request, 'about.html', {'name': 'Alejandro'})
 
